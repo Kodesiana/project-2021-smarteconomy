3a158cf4f6ce57017f6ab0ba6b41cc9441b39f3b
telkomdev-galang <galang.septiadi.dev@gmail.com>
2023-07-25 08:27:58 +0700

    feat(questionnaire): adjust question

--- frontend/src/components/elements/FragmentStepper/X1.tsx
+++ frontend/src/components/elements/FragmentStepper/X1.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Container, Group, Radio, Select, Text, Textarea, TextInput, Title } from "@mantine/core"
+import { Button, Card, Container, Group, NumberInput, Radio, Select, Table, Text, Textarea, TextInput, Title } from "@mantine/core"
 import { useCallback } from "react"
 import InputWrapper from "../InputWrapper"
 
@@ -20,6 +20,112 @@ const X1 = ({ data }: X1Props) => {
     <Container size="sm">
       <Card>
         <Title order={5}>Karakter Warga</Title>
+        <InputWrapper 
+          name="x1_1_i"
+          label="Usia"
+          Component={NumberInput}
+        />
+        <Text fw={600}>Pendidikan formal terakhir</Text>
+        <InputWrapper
+          name="X1_2a_i"
+          Component={(e: any) => <Radio.Group {...e} orientation="vertical" spacing="xs">
+            {[
+              {
+                label: 'SD',
+                value: '1'
+              },
+              {
+                label: 'SMP',
+                value: '2'
+              },
+              {
+                label: 'SMA',
+                value: '3'
+              },
+              {
+                label: 'Perguruan Tinggi',
+                value: '4'
+              }
+            ].map(({ label, value }) => (
+                <Radio key={`${label}-${value}`} label={label} value={value} />
+              ))}
+          </Radio.Group>}
+        />
+        <InputWrapper
+          name="X1_2b_i"
+          Component={(e: any) => <Radio.Group {...e} orientation="vertical" spacing="xs">
+            {[
+              {
+                label: 'Tamat',
+                value: '1'
+              },
+              {
+                label: 'Tidak Tamat',
+                value: '2'
+              },
+            ].map(({ label, value }) => (
+              <Radio key={`${label}-${value}`} label={label} value={value} />
+            ))}
+          </Radio.Group>}
+        />
+        <InputWrapper
+          name="x1_1_i"
+          label="Jika tidak tamat"
+          placeholder="Tahun"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Pelatihan yang pernah Bapak/Ibu ikuti terkait pemberdayaan masyarakat, sebutkan</Text>      
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Tahun</th>
+              <th>Nama pelatihan</th>
+              <th>Penyelenggara</th>
+              <th>Jumlah jam pertemuan</th>
+              <th>Metode</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...new Array(4)]
+              .map((_,i) => (
+                <tr>
+                  <td>
+                    <InputWrapper
+                      name={`x1_3a_${i+1}_i`}
+                      Component={NumberInput}
+                    />
+                  </td>
+                  <td>
+                    <InputWrapper
+                      name={`x1_3b_${i + 1}_i`}
+                      Component={TextInput}
+                    />
+                  </td>
+                  <td>
+                    <InputWrapper
+                      name={`x1_3c_${i + 1}_i`}
+                      Component={TextInput}
+                    />
+                  </td>
+                  <td>
+                    <InputWrapper
+                      name={`x1_3d_${i + 1}_i`}
+                      Component={TextInput}
+                    />
+                  </td>
+                  <td>
+                    <InputWrapper
+                      name={`x1_3e_${i + 1}_i`}
+                      Component={TextInput}
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+
         <InputWrapper
           name="x1_4"
           label="Pekerjaan yang dilakukan oleh bapak"
@@ -30,6 +136,199 @@ const X1 = ({ data }: X1Props) => {
               ))}
           </Radio.Group>}
         />
+
+        <InputWrapper
+          name="x1_4b_i"
+          Component={TextInput}
+          placeholder="Lainnya"
+        />
+
+        <InputWrapper
+          name="x1_5a_i"
+          label="Pekerjaan yang dilakukan oleh Ibu"
+          Component={(e: any) => <Radio.Group {...e} orientation="vertical" spacing="xs">
+            {handleFindData({ name: 'x1_4' })
+              ?.map((a: any) => (
+                <Radio key={`${a.label}-${a.value}`} {...a} />
+              ))}
+          </Radio.Group>}
+        />
+
+        <InputWrapper
+          name="x1_5b_i"
+          Component={TextInput}
+          placeholder="Lainnya"
+        />
+
+        <InputWrapper
+          name="x1_6_i"
+          label="Sudah berapa lama pekerjaan tersebut dilakukan oleh Bapak"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_7_i"
+          label="Sudah berapa lama pekerjaan tersebut dilakukan oleh Ibu"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Jumlah anak usia sekolah</Text> 
+
+        <InputWrapper
+          name="x1_8a_i"
+          placeholder="TK ... Orang"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_8b_i"
+          placeholder="SD ... Orang"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_8c_i"
+          placeholder="SMP ... Orang"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_8d_i"
+          placeholder="SMA ... Orang"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Usaha Tani</Text> 
+
+        <InputWrapper
+          name="x1_9_i"
+          label="Jumlah anak (mahasiswa)"
+          placeholder="Jumlah anak (mahasiswa) ... Orang"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_10_i"
+          label="Luas tanah kepemilikan m2"
+          placeholder="Luas tanah kepemilikan m2"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Status kepemilikan lahan</Text>
+
+        <InputWrapper
+          name="x1_11a_i"
+          label="Milik sendiri m2"
+          placeholder="Milik sendiri m2"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_11b_i"
+          label="Sewa m2"
+          placeholder="Sewa m2"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_11c_i"
+          label="Bagi hasil m2"
+          placeholder="Bagi hasil m2"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_11d_i "
+          label="Buruh tani m2"
+          placeholder="Buruh tani m2"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_12_i"
+          label="Jika lahan yang digarap adalah bukan 
+lahan milik sendiri, berapa upah 
+menggarap lahan tani tsb"
+          placeholder="Jika lahan yang digarap adalah bukan 
+lahan milik sendiri, berapa upah 
+menggarap lahan tani tsb"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Jika lahan yang digarap adalah milik
+          sendiri, adakah upah buruh yang
+          Bapak/Ibu keluarkan? Jika Ya, berapa
+          orang dan sebutkan jumlah
+          pengeluarannya</Text>
+
+        <InputWrapper
+          name="x1_13a_i"
+          label="Buruh"
+          placeholder=".... orang buruh"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="x1_13b_i"
+          label="Buruh"
+          placeholder="1 orang = ...... Rp/hari"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Jenis dan jumlah tanaman yang ditanam dalam satu periode produksi</Text>
+
+        <InputWrapper
+          name="X1_14a_1_i"
+          placeholder="Tanaman"
+          Component={TextInput}
+        />
+
+        <InputWrapper
+          name="X1_14b_1_i"
+          placeholder="Jumlah ..... kg"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          label="Jenis tanaman yang ditanam dalam satu tahun"
+          name="X1_15_i"
+          Component={TextInput}
+        />
+
+        <Text fw={600}>Berapa jumlah modal untuk satu kali periode musim</Text>
+
+        <InputWrapper
+          name="X1_16a_i"
+          placeholder="Benih"
+          label="Benih"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="X1_16b_i"
+          placeholder="Pupuk"
+          label="Pupuk"
+          Component={NumberInput}
+        />
+
+        <InputWrapper
+          name="X1_16c_i"
+          placeholder="Pestisida"
+          label="Pestisida"
+          Component={NumberInput}
+        />
+  
+        <InputWrapper
+          name="X1_16d_i"
+          placeholder="Lain-lain"
+          label="Lain-lain"
+          Component={NumberInput}
+        />
+
+        <Text fw={600}>Berapa jumlah upah dan tenaga kerja yang membantu usahatani Bapak/Ibu?</Text>
+
+        
+
         <InputWrapper
           name="x1_19"
           label="Status kepemilikan ternak"
